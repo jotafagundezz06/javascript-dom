47aa5e78aca1f299a123630304206115dbdbce5c
jotafagundezz06 <jotafagundezz@gmail.com>
2023-09-22 19:36:26 -0300

    versao 004 - foi colocado musica.

--- js/script.js
+++ js/script.js
@@ -4,18 +4,36 @@ const curtoBt = document.querySelector('.app__card-button--curto'); // constante
 const longoBt = document.querySelector('.app__card-button--longo'); // constante que vai pegar o botao de descanso longo do html
 const banner = document.querySelector('.app__image'); // selecionar a imagem da pagina 
 const titulo = document.querySelector('.app__title'); // seleciona a classe de titulo da pagina
+const butom = document.querySelectorAll('.app__card-button'); // seleciona todas as classes de botao que tem na pagina
+const musicaFocoInput = document.querySelector('#alternar-musica') // seleciona o imput da musica
+let musica = new Audio('/sons/luna-rise-part-one.mp3'); // varivel que vai armazenar o som
+musica.loop = true // vai musica vai tocar em um loop 
+
+musicaFocoInput.addEventListener('change',function () { // evento de ligar a musica 
+    if(musica.paused) { // se a musica estiver pausada 
+            musica.play() // de o play
+        } else {  // se nao 
+            musica.pause() // pause a musica 
+        }
+})
 
 focoBt.addEventListener('click', () => { // vai criar um evento de click no botao de foco 
     alterarContexto('foco') // pega a classe de foco
+    focoBt.classList.add('active'); // ativa o desenho de ativado do botao 
 })
 curtoBt.addEventListener('click', () => { // vai criar um evento de click no botao de foco curto
     alterarContexto('descanso-curto') // pega a classe de descanso curto
+    curtoBt.classList.add('active'); // ativa o desenho de ativado do botao 
 })
 longoBt.addEventListener('click', () => { // vai criar um evento de click no botao de foco longo
     alterarContexto('descanso-longo') // pega a classe de descanso longo
+    longoBt.classList.add('active'); // ativa o desenho de ativado do botao
 })
 
 function alterarContexto (contexto){ // funçao que vai alguerar a cor e a iamgem da pagina
+    butom.forEach(function(contexto){
+    contexto.classList.remove('active') // vai remover de acordo com o contexto
+    });
     html.setAttribute('data-contexto', contexto ); // pega o data contesto e muda acor da pagina pela a classe
     banner.setAttribute('src', `/imagens/${contexto}.png`); // pega o src e muda a imagem 
     switch (contexto) {
